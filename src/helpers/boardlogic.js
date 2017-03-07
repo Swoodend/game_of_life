0e36c6be6b08c05068daceab32ae2259c2e49753
Scott Woodend <woodendscott@gmail.com>
2017-03-07 17:09:54 -0500

    create a function that will create a deep copy of an array. It will be used in order update board status for each generation

--- src/helpers/boardlogic.js
+++ src/helpers/boardlogic.js
@@ -6,4 +6,17 @@ function setRandomState(board){
   });
 }
 
+function copyArr(arr){
+  let newArr = arr.slice();
+  for (let i = 0; i < arr.length; i++){
+    if (Array.isArray(arr[i])){
+      newArr[i] = copyArr(arr[i]);
+    } else {
+      newArr[i] = arr[i]
+    }
+  }
+  return newArr;
+}
+
+
 export default setRandomState
