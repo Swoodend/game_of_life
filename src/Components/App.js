7b3502e1291a4043f501ea8091791db4a42bd535
Scott Woodend <Scott Woodend>
2017-03-08 16:07:28 -0500

    when the start button is clicked, a generation of the game is drawn (for now, later this will start the main loop

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,11 +3,21 @@ import Board from './Board';
 import "../../public/styles/App.css";
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.refs.board.tick();
+  }
+
   render() {
     return (
       <div className="app">
-        <Board />
-        <div className="start-button">Start</div>
+        <Board ref="board"/>
+        <div onClick={this.handleClick} className="start-button">Start</div>
       </div>
     );
   }
