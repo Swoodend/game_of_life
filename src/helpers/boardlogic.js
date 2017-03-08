10d316ba81125c39eb3b6ac32ea30d2fd0565638
Scott Woodend <Scott Woodend>
2017-03-08 16:08:37 -0500

    only render cells that are not on the edge

--- src/helpers/boardlogic.js
+++ src/helpers/boardlogic.js
@@ -1,7 +1,12 @@
-function setRandomState(board){
-  return board.map((row) => {
-    return row.map((cell) => {
-      return Math.random() > 0.5 ? 1 : 0;
+function getRandomState(board){
+  return board.map((row, i) => {
+    return row.map((cell, j) => {
+      //only consider birthing the cell if it isnt an edge piece (ignoring the edges for this implementation)
+      if ((i !== 0 && i !== board.length -1) && (j !== 0 && j!==board[i].length -1)){
+        return Math.random() > 0.85 ? 1 : 0;
+      } else {
+        return 0;
+      }
     });
   });
 }
@@ -19,4 +24,4 @@ function copyArr(arr){
 }
 
 
-export default setRandomState
+export { getRandomState, copyArr }
