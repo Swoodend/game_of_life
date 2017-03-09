47e7814645933888cb4427b84185f94c7a0413a0
Scott Woodend <Scott Woodend>
2017-03-09 09:24:28 -0500

    getRandomState() is not needed anymore as the state of the board is set when it is first being built

--- src/helpers/boardlogic.js
+++ src/helpers/boardlogic.js
@@ -1,16 +1,3 @@
-function getRandomState(board){
-  return board.map((row, i) => {
-    return row.map((cell, j) => {
-      //only consider birthing the cell if it isnt an edge piece (ignoring the edges for this implementation)
-      if ((i !== 0 && i !== board.length -1) && (j !== 0 && j!==board[i].length -1)){
-        return Math.random() > 0.85 ? 1 : 0;
-      } else {
-        return 0;
-      }
-    });
-  });
-}
-
 function copyArr(arr){
   let newArr = arr.slice();
   for (let i = 0; i < arr.length; i++){
@@ -24,4 +11,4 @@ function copyArr(arr){
 }
 
 
-export { getRandomState, copyArr }
+export default copyArr;
